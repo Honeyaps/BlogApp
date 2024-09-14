b2bad6d640b8e2f95ac93290ef916b7fcab8b0fb
Hemant Singh <honeyaps12345@gmail.com>
2024-09-14 20:23:46 +0530

    deploy versel

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,34 @@ const userRouter = require("./routes/user");
 const blogRouter = require("./routes/blog");
 const app = express();
 
-app.use(cors({
-  origin: true,
-}));
+// app.use(cors({
+//   AccessControlAllowOrigin: req.headers.origin,
+//       AccessControlAllowHeaders: "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name, Origin",
+//       AccessControlAllowMethods: "POST, GET, PUT, DELETE, OPTIONS",
+//       AccessControlAllowCredentials: "true",
+// }));
+
+app.use(function (req, res, next) {
+  var responseSettings = {
+    AccessControlAllowOrigin: req.headers.origin,
+    AccessControlAllowHeaders: "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name, Origin",
+    AccessControlAllowMethods: "POST, GET, PUT, DELETE, OPTIONS",
+    AccessControlAllowCredentials: "true",
+  };
+
+  res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+  res.header("Access-Control-Allow-Origin", responseSettings.AccessControlAllowOrigin);
+  res.header("Access-Control-Allow-Headers", req.headers["access-control-request-headers"] ? req.headers["access-control-request-headers"] : "x-requested-with");
+  res.header("Access-Control-Allow-Methods", req.headers["access-control-request-method"] ? req.headers["access-control-request-method"] : responseSettings.AccessControlAllowMethods);
+  if ("OPTIONS" == req.method) {
+    res.sendStatus(200).end();
+  } else {
+    next();
+  }
+});
+
+
+
 app.use(express.json());
 
 // for registration
