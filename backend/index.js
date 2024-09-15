ad69cc876f3ee979985d4b272af64211b5dc1a96
Hemant Singh <honeyaps12345@gmail.com>
2024-09-15 14:09:09 +0530

    deploy versel

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mainRouter = require("./mainRouter");
 const app = express();
 
+require("dotenv").config();
 
 app.use(cors({
   origin: '*',
@@ -13,14 +14,23 @@ app.use(cors({
 
 app.use(express.json());
 
+// Use mainRouter under /v1
 app.use("/v1", mainRouter);
 
+// Basic route for health check
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("Server is running");
 });
 
-app.listen(4500, () => {
-  console.log("port connected");
+// Catch-all for undefined routes
+app.use((req, res, next) => {
+  res.status(404).send({ error: "Not Found" });
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: "Something went wrong!" });
+});
 
+module.exports = app;
