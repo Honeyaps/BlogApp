ec9e8c0ccde689ce0b9775e7186ccb7c6e0d3fb1
Hemant Singh <honeyaps12345@gmail.com>
2024-09-15 13:12:07 +0530

    add versel.json

--- frontend/src/User/axiosConfig.jsx
+++ frontend/src/User/axiosConfig.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// const axiosInstance = axios.create({
-//   baseURL: "https://blogger-api-kappa.vercel.app"
-// });
+const axiosInstance = axios.create({
+  baseURL: "https://blogger-api-kappa.vercel.app/v1",
+});
 
 // local
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:4500/v1"
-});
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:4500/v1"
+// });
 
 export default axiosInstance;
