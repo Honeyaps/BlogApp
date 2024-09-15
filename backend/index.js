9a1cbffac95836af898b402e363f51ed22f1ff5d
Hemant Singh <honeyaps12345@gmail.com>
2024-09-15 13:50:14 +0530

    deploy versel

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const mainRouter = require("./mainRouter");
 const app = express();
-const {User} = require("./db");
-const zod = require("zod");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-require("dotenv").config();
 
 
 app.use(cors({
@@ -20,49 +14,6 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/v1", mainRouter);
-
-
-const sigupValidation = zod.object({
-  firstname: zod.string(),
-  lastname: zod.string(),
-  username: zod.string(),
-  email: zod.string().email(),
-  password: zod.string().min(6),
-});
-
-app.post("/signin", async (req, res) => {
-  const body = req.body;
-  const success = sigupValidation.safeParse(body);
-
-  if (!success) {
-    return res.status(403).json({ msg: "data is not valid" });
-  }
-
-  try {
-    const checks = await User.findOne({
-      email: body.email,
-    });
-
-    if (!checks) {
-      return res.status(403).json({ msg: "enter correct email" });
-    }
-
-    const passCmpr = await bcrypt.compare(body.password, checks.password);
-    if (passCmpr) {
-      const token = jwt.sign(checks._id.toHexString(), process.env.SECRET);
-
-      return res.json({
-        name: checks.firstname,
-        token: token
-      });
-    } else {
-      return res.status(403).json({ msg: "incorrect password" });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(404).json({ msg: "error while signing in" });
-  }
-});
 
 app.get("/", (req, res) => {
   res.send("hello");
